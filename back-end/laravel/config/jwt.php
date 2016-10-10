a538ecc81993e3509648f06788be6dc2a6807e5c
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-10 14:47:13 +0200

    Laravel sync met Edward

--- back-end/laravel/config/jwt.php
+++ back-end/laravel/config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'UgY4AbGbZEXOQRZsv3pXsk9bGHseGK6d'),
 
     /*
     |--------------------------------------------------------------------------

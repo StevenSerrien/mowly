853656c9a4e6149dc50a4e412e2ceb2510babc21
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-10-10 15:55:37 +0200

    Migration for places
    Plus jwt token length changed

---
+++ back-end/laravel/database/migrations/2016_10_10_134434_create_places_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('streetname');
+            $table->string('housenumber', 15);;
+            $table->string('city');
+            $table->string('country', 50);
+            $table->string('latitude');
+            $table->string('longitude');
+
+            $table->integer('user_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('places');
+    }
+}

4cf552f350e78f4db73e2ec54e70a186228c18ac
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-10 16:14:09 +0200

    Create table foods

---
+++ back-end/laravel/database/migrations/2016_10_10_134946_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 10, 2);
+
+
+            $table->integer('place_id')->index();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('foods');
+    }
+}

bea47eda8196872b6773dbefdfd2e61ffa96a2f4
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-11 16:34:05 +0200

    Models update

--- back-end/laravel/app/Drink.php
+++ back-end/laravel/app/Drink.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Food extends Model
+class Drink extends Model
 {
   use SoftDeletes;
   protected $fillable = ['name', 'description', 'price'];
--- back-end/laravel/app/Place.php
+++ back-end/laravel/app/Place.php
@@ -10,4 +10,15 @@ class Place extends Model
     use SoftDeletes;
     protected $fillable = ['name', 'streetname', 'housenumber', 'city', 'country', 'latitude', 'longitude'];
     protected $dates = ['deleted_at'];
+
+
+    public function drinks()
+    {
+      return $this->hasMany('App\Place');
+    }
+
+    public function foods()
+    {
+      return $this->hasMany('App\Food');
+    }
 }

209d061eeccf57c0c17157476fc3a5ae93ae2ed0
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-11 17:06:17 +0200

    Add controller + fill - DrinkController and FoodController

--- back-end/laravel/app/Api/V1/Controllers/DrinkController.php
+++ back-end/laravel/app/Api/V1/Controllers/DrinkController.php
@@ -1,12 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Api\V1\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use JWTAuth;
+use App\Place;
+use App\Drink;
+use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
 
 class DrinkController extends Controller
 {
-    //
+    use Helpers;
 }
--- back-end/laravel/app/Api/V1/Controllers/FoodController.php
+++ back-end/laravel/app/Api/V1/Controllers/FoodController.php
@@ -1,12 +1,41 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Api\V1\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use JWTAuth;
+use App\Place;
+use App\Food;
+use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+//FindorFail error catch
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+
 
 class FoodController extends Controller
 {
-    //
+    use Helpers;
+
+    public function store(Request $request)
+    {
+      // Check if place_id sent with POST exists in database
+      // If fails then ModelNotFoundException
+      $place = Place::findOrFail($request->get('place_id'));
+
+      //Creates new databaserecord in foodtable with info in URL
+      $food = new Food;
+      $food->name = $request->get('name');
+      $food->description = $request->get('description');
+      $food->price = $request->get('price');
+
+
+      if($place->foods()->save($food))
+      //Returns foodobject on completion
+      return $food;
+      else
+      return $this->response->error('could_not_create_food', 500);
+    }
 }

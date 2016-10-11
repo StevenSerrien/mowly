4e7f448ee8575c79420c0d4ec0db736f5c78ea01
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-11 17:16:12 +0200

    Update DrinkController

--- back-end/laravel/app/Api/V1/Controllers/DrinkController.php
+++ back-end/laravel/app/Api/V1/Controllers/DrinkController.php
@@ -15,5 +15,25 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DrinkController extends Controller
 {
-    use Helpers;
+  use Helpers;
+
+  public function store(Request $request)
+  {
+    // Check if place_id sent with POST exists in database
+    // If fails then ModelNotFoundException
+    $place = Place::findOrFail($request->get('place_id'));
+
+    //Creates new databaserecord in foodtable with info in URL
+    $drink = new Drink;
+    $drink->name = $request->get('name');
+    $drink->description = $request->get('description');
+    $drink->price = $request->get('price');
+
+
+    if($place->drinks()->save($drink))
+    //Returns foodobject on completion
+    return $drink;
+    else
+    return $this->response->error('could_not_create_drink', 500);
+  }
 }

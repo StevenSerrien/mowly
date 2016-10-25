3c6c0beba79d3937100c9dcbadee4f3664590a36
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-10-25 17:30:57 +0200

    Delete dist rommel

--- front-end/dist/models/food.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Food = (function () {
-    function Food() {
-    }
-    return Food;
-}());
-exports.Food = Food;
-//# sourceMappingURL=food.js.map
--- front-end/dist/services/drink.service.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=drink.service.js.map

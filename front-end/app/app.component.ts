61fb08c04dec49e83ee00b7a7b4a38b4dc0e4cb7
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-25 13:42:59 +0200

    Angular2 component structure fix

--- front-end/app/app.component.ts
+++ front-end/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from './search/search.component';
+
 @Component({
   selector: 'mowly-app',
   templateUrl: 'app/main.component.html'
---
+++ front-end/app/search/search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-component',
+  templateUrl: 'app/search/search.component.html',
+  styleUrls:['app/search/search.component.css'],
+})
+export class SearchComponent { }

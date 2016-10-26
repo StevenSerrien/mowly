d184d9c16ca83f281b1c1d3052297b412641dd12
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-26 17:52:08 +0200

    commitje

---
+++ front-end/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+    //moduleId: module.id,
+    selector: 'login-component',
+    templateUrl: 'app/login/login.component.html',
+    styleUrls:['app/login/login.component.css'],
+})
+
+export class LoginComponent {
+    model: any = {};
+    loading = false;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
+
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.login('stevenserrien@gmail.com', 'test123');
+        this.authenticationService.logout();
+    }
+
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.email, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate(['']);
+                },
+                error => {
+
+                    this.loading = false;
+                });
+    }
+}
--- front-end/app/services/authentication.service.ts
+++ front-end/app/services/authentication.service.ts
@@ -9,10 +9,12 @@ export class AuthenticationService {
     constructor(private http: Http) { }
 
     login(email, password) {
+
         return this.http.post(`${AppSettings.API_ENDPOINT}/auth/login`, JSON.stringify({ email: email, password: password }))
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let user = response.json();
+                console.log('hallo');
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));

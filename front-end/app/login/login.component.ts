efe9cd7311e3c42e4ae6516048358e407288f1e3
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-10-26 21:21:41 +0200

    Login werkt token wordt opgevraagd en getoond ;-)

--- front-end/app/login/login.component.ts
+++ front-end/app/login/login.component.ts
@@ -4,36 +4,39 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
-    //moduleId: module.id,
-    selector: 'login-component',
-    templateUrl: 'app/login/login.component.html',
-    styleUrls:['app/login/login.component.css'],
+  //moduleId: module.id,
+  selector: 'login-component',
+  templateUrl: 'app/login/login.component.html',
+  styleUrls:['app/login/login.component.css'],
 })
 
 export class LoginComponent {
-    model: any = {};
-    loading = false;
+  model: any = {};
+  loading = false;
+  token: string;
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService) { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService) { }
 
     ngOnInit() {
-        // reset login status
-        this.authenticationService.login('stevenserrien@gmail.com', 'test123');
-        //this.authenticationService.logout();
+
+      // reset login status
+      // this.authenticationService.login('stevenserrien@gmail.com', 'test123');
+      // this.authenticationService.logout();
+      // this.login('edward@edward.edward', '123456789');
     }
 
-    login() {
-        this.loading = true;
-        this.authenticationService.login(this.model.email, this.model.password)
-            .subscribe(
-                data => {
-                    this.router.navigate(['']);
-                },
-                error => {
-
-                    this.loading = false;
-                });
+    login(email: string, password: string): void {
+      email = email.trim();
+      if (!email) { return; }
+      this.authenticationService.login(email, password)
+      .then(data => {
+        if(!data){console.log('tis kapot');}
+        else {
+          this.token = data['token'];
+          console.log(data);
+        }
+      });
     }
-}
+  }
--- front-end/app/services/authentication.service.ts
+++ front-end/app/services/authentication.service.ts
@@ -1,34 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Observable';
 import {AppSettings} from '../appSettings';
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: Http) { }
-
-    login(email, password) {
-      const headers = new Headers();
-        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+  constructor(private http: Http) { }
+  private headers = new Headers({'Content-Type': 'application/json'});
 
 
-        return this.http.post(`${AppSettings.API_ENDPOINT}/auth/login`, JSON.stringify({ email: email, password: password }), {headers})
-            .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                let user = response.json();
-                console.log('hallo');
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
-                    console.log("logged in");
-                    console.log(user.token);
-                }
-            });
-    }
+  login(gemail: string, gpassword: string): Promise<string> {
+    console.log(gemail + ' ' + gpassword);
+    return this.http
+    .post(`${AppSettings.API_ENDPOINT}/auth/login`, JSON.stringify({email: gemail, password: gpassword}), {headers: this.headers})
+    .toPromise()
+    .then(res => res.json())
+    .catch(this.handleError);
+  }
 
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-    }
+  private handleError (error: Response | any) {
+  // In a real world app, we might use a remote logging infrastructure
+  let errMsg: string;
+  if (error instanceof Response) {
+    const body = error.json() || '';
+    const err = body.error || JSON.stringify(body);
+    errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+  } else {
+    errMsg = error.message ? error.message : error.toString();
+  }
+  console.error(errMsg);
+  return Promise.reject(errMsg);
+}
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+  }
 }

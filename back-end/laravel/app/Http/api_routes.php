7c2e770a33ed6caf9f9d2aec4559c55af1d29007
edwwaarrdd <edward.vereertbrugghen@gmail.com>
2016-10-27 11:02:24 +0200

    Cors sun

--- back-end/laravel/app/Http/api_routes.php
+++ back-end/laravel/app/Http/api_routes.php
@@ -11,22 +11,22 @@ $api->version('v1', function ($api) {
 
 
 	//routing where log in is needed
-	$api->group(['middleware' => 'cors'], function ($api) {
+	// $api->group(['middleware' => 'cors'], function ($api) {
 		$api->post('auth/login', 'App\Api\V1\Controllers\AuthController@login');
 		$api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
 		$api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery');
 		$api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset');
-	});
+	// });
 
 	//routing where log in is needed
-	$api->group(['middleware' => ['api.auth', 'cors']], function ($api) {
+	$api->group(['middleware' => ['api.auth']], function ($api) {
 		$api->post('place/store', 'App\Api\V1\Controllers\PlaceController@store');
 		$api->post('food/store', 'App\Api\V1\Controllers\FoodController@store');
 		$api->post('drink/store', 'App\Api\V1\Controllers\DrinkController@store');
 	});
 
 	//routing where log in is not needed.
-	$api->group(['middleware' => 'cors'], function ($api) {
+	// $api->group(['middleware' => 'cors'], function ($api) {
 		$api->get('places/{id}', 'App\Api\V1\Controllers\PlaceController@show');
 		$api->get('places', 'App\Api\V1\Controllers\PlaceController@index');
 		$api->get('foods', 'App\Api\V1\Controllers\FoodController@index');
@@ -43,8 +43,8 @@ $api->version('v1', function ($api) {
 		$api->get('place/searchwithlocation', 'App\Api\V1\Controllers\PlaceController@searchPlacesWithLocation');
 		//searches for places WITHOUT query nearby user and returns them.
 		$api->get('place/searchnearbylocation', 'App\Api\V1\Controllers\PlaceController@searchNearbyLocation');
-		
-	});
+
+	// });
 
 
 });
